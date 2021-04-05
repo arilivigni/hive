6a540f9606c7867befd2888b6fef35edc9f05346
Ari LiVigni <arilivigni@gmail.com>
2021-04-05 08:54:25 -0400

    updated app-sre buildah deploy script

--- hack/app_sre_buildah_deploy.sh
+++ hack/app_sre_buildah_deploy.sh
@@ -10,8 +10,8 @@ CURRENT_DIR=$(dirname $0)
 # build image
 buildah bud --tls-verify=$SSL_VERIFY \
 --layers \
--f $DOCKERFILE_CONTEXT_DIR \
--t $QUAY_IMG:$GIT_HASH .
+-f $DOCKERFILE_CONTEXT_DIR/Dockerfile \
+-t $QUAY_IMG:$GIT_HASH $DOCKERFILE_CONTEXT_DIR
 
 # deploy image with to quay.io with git hash to image tag
 buildah push --tls-verify=$SSL_VERIFY \
