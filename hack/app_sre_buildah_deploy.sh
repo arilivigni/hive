894af0748acd726e8ab84ac3169142b26b41ad9e
Ari LiVigni <arilivigni@gmail.com>
2021-04-07 21:08:01 -0400

    updated with storage change to buildah

--- hack/app_sre_buildah_deploy.sh
+++ hack/app_sre_buildah_deploy.sh
@@ -8,17 +8,20 @@ set -exv
 CURRENT_DIR=$(dirname $0)
 
 # build image
-buildah bud --tls-verify=$SSL_VERIFY \
+buildah bud --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 --layers \
 -f $DOCKERFILE_CONTEXT_DIR/Dockerfile \
 -t $QUAY_IMG:$GIT_HASH $DOCKERFILE_CONTEXT_DIR
 
 # deploy image with to quay.io with git hash to image tag
-buildah push --tls-verify=$SSL_VERIFY \
+buildah push --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 $QUAY_IMG:$GIT_HASH \
 $QUAY_IMG:$GIT_HASH
 
 # deploy image to quay.io with latest tag
-buildah push --tls-verify=$SSL_VERIFY \
+buildah push --storage-driver=vfs \
+--tls-verify=$SSL_VERIFY \
 $QUAY_IMG:$GIT_HASH \
 $QUAY_IMG:latest
